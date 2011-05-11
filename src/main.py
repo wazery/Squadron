02a1afd63a3a1a48d6aa20b6a99653f352cefee0
Menopia <al.wazery@gmail.com>
2011-05-11 04:18:06 +0200

    Added load_sound function to load all game sounds

--- src/main.py
+++ src/main.py
@@ -44,6 +44,18 @@ def load_image(filename, colorkey=None):
 	
 	return image, image.get_rect()
 
-
+def load_sound(name):
+	class No_Sound:
+		def play(self) : pass
 	
-
+	if not pygame.mixer or not pygame.mixer.get_init():
+		return No_Sound()
+	
+	fullname = os.path.join('data', name)
+	if os.path.exists(fullname) == False:
+		sound = pygame.mixer.Sound(fullname)
+	else
+		print 'File %s does not exist!', filename
+		return No_Sound()
+	
+	return sound
