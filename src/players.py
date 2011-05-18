337dea570c7851338419dd18082c1e23d05c2fc4
Menopia <al.wazery@gmail.com>
2011-05-18 21:07:04 +0200

    Small fixes in the players module

--- src/players.py
+++ src/players.py
@@ -23,46 +23,49 @@
 import loader
 import pygame
 from pygame.locals import *
+
 class Players(pygame.sprite.Sprite):
 	"""This class is for the game players"""
-	def init(slef, screen_width, screen_height, down=False, side= False):
-		pygame.sprite.Sprite.__init__(self) #call spirit initializer
-		self.screen_width = screen_width
-		self.screen_height = screen_height
+    def init(slef, screen_width, screen_height, bottom=False, side= False):
+	pygame.sprite.Sprite.__init__(self) #call sprite initializer
+	self.screen_width = screen_width
+	self.screen_height = screen_height
 
-		if down is True:
-			self.down = True 
-			self.image, self.rect = loader.load_iamge('player_down.png', -1)
+	if bottom is True:
+	    self.bottom = True 
+	    self.image, self.rect = loader.load_iamge('player_bottom.png', -1)
+	    self.rect = self.image.get_rect(topleft = (start_x, start_y))
 
-		if side is True:
-			self.side = True
-			self.image, self.rect = loader.load_iamge('player_side.png', -1)
+	if side is True:
+	    self.side = True
+	    self.image, self.rect = loader.load_iamge('player_side.png', -1)
+	    self.rect = self.image.get_rect(topleft = (start_x, start_y))
 
-	def update(self):
-		#Get the current keyboard pressed button (state)
-		keystate = pygame.key.get_pressed() 
+    def update(self):
+	#Get the current keyboard pressed button (state)
+        keystate = pygame.key.get_pressed() 
 
-		if self.down is True: #If player is down corner player, and it reaches te lower bound, reset the location
-			if slef.rect.left < 0: #Check if the player is out of boundary
-				self.rect.left = 0
-			elif slef.rect.right > 800:
-				self.rect.right = 800
+	if self.bottom is True: #If player is bottom corner player, and it reaches te lower bound, reset the location
+	    if slef.rect.left < 0: #Check if the player is out of boundary
+		self.rect.left = 0
+	    elif slef.rect.right > 800:
+		self.rect.right = 800
 
-			if keystate == [pygame.K_LEFT]:
-				self.rect.move_ip(-7,0)
-			if keystate == [pygame.K_RIGHT]:
-				self.rect.move_ip(7,0)
+	    if keystate == [pygame.K_LEFT]:
+		self.rect.move_ip(-7,0)
+	    if keystate == [pygame.K_RIGHT]:
+		self.rect.move_ip(7,0)
 
-		if self.side is True:
-			if slef.rect.bottom < 0:
-				self.rect.bottom = 0
-			elif slef.rect.top > 600:
-				self.rect.top = 600
+	if self.side is True:
+	    if slef.rect.bottom < 0:
+		self.rect.bottom = 0
+	    elif slef.rect.top > 600:
+		self.rect.top = 600
 
-			if keystate == [pygame.K_UP]:
-				self.rect.move_ip(0,-7)
-			if keystate == [pygame.K_DOWN]:
-				self.rect.move_ip(7,0)
+	    if keystate == [pygame.K_UP]:
+		self.rect.move_ip(0,-7)
+	    if keystate == [pygame.K_DOWN]:
+		self.rect.move_ip(7,0)
 	
-		screen_rect = Rect(0, 0, self.screen_width, self.screen_height)
-		self.rect.clamp_ip(SCREENRECT)
+    screen_rect = Rect(0, 0, self.screen_width, self.screen_height)
+    self.rect.clamp_ip(SCREENRECT)
